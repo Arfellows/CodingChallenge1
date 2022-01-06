00126da9d0bd1ffe0259085a7e4ed87791ae486f
Ashley Fellows <ashley.r.fellows@gmail.com>
2022-01-05 19:17:02 -0500

    initial commit

---
+++ CodingChallengeDay1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallengeDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var listA = new[] { "abc", "xyz", "klm", "xyz", "abc", "abc", "rst" };
+            var listB = new[] { "abc", "xyz", "klm", "rst" };
+            var listC = listB.Where(x => listA.Where(y => y == x).Count() == 1);
+            foreach(var item in listC)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
